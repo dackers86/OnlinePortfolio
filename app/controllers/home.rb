fea7d9c53f236de9b95893a7e5aecf0cfe1380f1
Darren Ackers <ackers86@hotmail.com>
2016-03-04 09:11:35 +0000

    Added reference bug for users

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -1,18 +1,18 @@
-
+require_relative "../lib/user"
 class HomeController < ApplicationController
 
   attr_accessor :current_user
 
   get "/" do
 
-    if session["current_user"].nil? then
+    if !session["current_user"].nil? then
       erb :home, locals: { current_user: session["current_user"]  }
     else
       erb :login
     end
   end
 
-  get '/auth/twitter/callback' do
+  get '/auth/:provider/callback' do
     session["current_user"] = User.from_omniauth(request.env['omniauth.auth'])
     redirect to('/')
   end
