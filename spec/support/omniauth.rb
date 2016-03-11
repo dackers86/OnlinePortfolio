c179e32ec85a9ffd3373db287dc2f8255ae714ab
Darren Ackers <ackers86@hotmail.com>
2016-03-11 08:48:29 +0000

    Updated omniauth test spec

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -2,5 +2,6 @@ OmniAuth.config.test_mode = true
 
 OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
   :provider => 'twitter',
-  :uid => '123545'
+  :uid => '123545',
+  :info => { :name => 'example' }
 })
